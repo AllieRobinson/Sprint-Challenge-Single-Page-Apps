fecb53fce6712ce7ca4ef0ca1733220d5eb165a1
Allie Robinson <allie-rae@Allies-MacBook.local>
2019-09-16 14:36:58 -0500

    Added homepage component to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 
@@ -10,6 +11,7 @@ export default function App() {
     <main>
       <Header />
       <TabNav />
+      <WelcomePage />
     </main>
   );
 }
